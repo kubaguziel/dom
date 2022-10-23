6b65c207e9ffb06dcc3055a2b29863ee57035a67
kubaguziel <jakub.guziel@onet.pl>
2022-10-23 15:32:10 +0200

    proba

---
+++ stopnie/stopnie.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+
+int main()
+{
+	long int f;
+	float c=0.00;
+	
+	std::cin >> f;
+	if (f >= -459 && f <= 1000000000)
+	{
+		c = (f-32)*(5/9);
+		std::cout.precision(3);
+		std::cout << c;
+	}
+	
+}
