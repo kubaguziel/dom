8845a69917e51c385fa2c6c9f44fe65ea0f5f904
kubaguziel <jakub.guziel@onet.pl>
2022-10-23 14:42:27 +0200

    g

--- tablice/tablice.cpp
+++ tablice/tablice.cpp
@@ -1,6 +1,17 @@
 ﻿#include <iostream>
 
+float oceny[5];
+float suma=0, srednia;
+
 int main()
 {
-    std::cout << "CO tam!\n";
+
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout << "Podaj ocene: ";
+		std::cin >> oceny[i];
+		suma += oceny[i];
+	}
+	srednia = suma / 5;
+	std::cout << "Srednia wynosi: " << srednia;
 }
