7a694588e4bbf10d6daf17629303ed8090150346
kubaguziel <jakub.guziel@onet.pl>
2022-10-23 15:02:43 +0200

    gotowe

---
+++ Przestepnosc/Przestepnosc.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+    int rok;
+    std::cout << "Podaj rok: ";
+    std::cin >> rok;
+    if (rok > 1 && rok < 8000)
+    {
+        if (rok % 4 == 0)
+        {
+            if (rok % 100 == 0 && rok % 400 != 0)
+            {
+                std::cout << "NIE";
+            }
+            else std::cout << "TAK";
+        }
+        else std::cout << "NIE";
+    }
+    else std::cout << "blad";
+}
