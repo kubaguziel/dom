7fc4ac1699dec07645866e2ce907d777504a9a5a
kubaguziel <jakub.guziel@onet.pl>
2022-10-31 16:52:29 +0100

    1c_blad

---
+++ Lamanieczekolady/Lamanieczekolady.cpp
@@ -0,0 +1,24 @@
+﻿// Lamanieczekolady.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+
+int main()
+{
+    unsigned int n, m,k;
+    std::cin >> n;
+    std::cin >> m;
+    if (n>=1&&n<=1000000&&m>=1&&m<=1000000)
+    {
+        std::cin >> k;
+        if(k>=1&&k<=1000000)
+        {
+            if (k % n == 0 || k % m == 0)
+            {
+                std::cout << "TAK";
+            }
+            else std::cout << "NIE";
+        }
+        
+    }
+}
