0a2acd590677105880c55391710307283aabad49
kubaguziel <jakub.guziel@onet.pl>
2022-10-31 16:12:57 +0100

    gotowe

---
+++ szachy liczba pol/szachy liczba pol.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    int n,biale,czarne;
+    std::cin>>n;
+    if (n >= 1 && n <= 1000)
+    {
+        if (n % 2 == 0)
+        {
+            biale = (n * n) / 2;
+            czarne = (n * n) / 2;
+        }
+        else
+        {
+            czarne = ((n * n) + 1) / 2;
+            biale = (n * n) - czarne;
+        }
+        std::cout << biale << std::endl;
+        std::cout << czarne << std::endl;
+    }
+}
