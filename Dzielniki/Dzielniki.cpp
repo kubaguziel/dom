aa076085902389ed254ce06313343707ad45fd89
kubaguziel <jakub.guziel@onet.pl>
2022-10-31 18:03:32 +0100

    niegotowe

---
+++ Dzielniki/Dzielniki.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main()
+{
+    long int n,dzielnik;
+    std::cin >> n;
+    if (n>0&&n<=1000000000000)
+    {
+        for (int i = n; i > 0; i--)
+        {
+            if ((n % i) == 0)
+            {
+                dzielnik = n / i;
+                std::cout << dzielnik << std::endl;
+            }
+
+        }
+    }
+    
+}
