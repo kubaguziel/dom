240cc7dda3961135e9e5fc3d3b37372e77d85d00
kubaguziel <jakub.guziel@onet.pl>
2022-10-31 17:27:17 +0100

    gotowe

---
+++ szachy_wieza/szachy_wieza.cpp
@@ -0,0 +1,24 @@
+﻿// szachy_wieza.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+
+int main()
+{
+	unsigned int x1,y1,x2,y2;
+	std::cin >> x1 >> y1;
+	if (x1 >= 1 && x1 <= 8 && y1 >= 1 && y1 <= 8)
+	{
+		std::cin >> x2 >> y2;
+		if (x2 >= 1 && x2 <= 8 && y2 >= 1 && y2 <= 8)
+		{
+			if ((x1 != x2 && y1 != y2)|| (x2 == x1 && y2 == y1))
+			{
+				std::cout << "NIE";
+			}
+			else std::cout << "TAK";
+		}
+	}
+
+
+}
