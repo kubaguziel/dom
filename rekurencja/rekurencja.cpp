5782fca8fa72ebd4354e7098f1404fdeaf8e5d0a
kubaguziel <jakub.guziel@onet.pl>
2022-11-19 19:26:07 +0100

    fibonaci

---
+++ rekurencja/rekurencja.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <iomanip>
+
+long int fib(long int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+    std::cout << fib(5);
+}
