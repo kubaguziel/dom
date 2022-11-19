3badd2ae108390aa8d1b78b5d05406b84961891d
kubaguziel <jakub.guziel@onet.pl>
2022-11-19 19:38:19 +0100

    fib,silnia,potega

--- rekurencja/rekurencja.cpp
+++ rekurencja/rekurencja.cpp
@@ -1,13 +1,31 @@
 ﻿#include <iostream>
 #include <iomanip>
 
-long int fib(long int n)
+long int fib( int n)
 {
     if (n == 1 || n == 2) return 1;
     else return fib(n - 1) + fib(n - 2);
 }
 
+long int silnia(int n)
+{
+    if (n == 0) return 1;
+    else return n * silnia(n - 1);
+}
+
+long int potega(int p, int w)
+{
+    if (w == 0) return 1;
+    else return p* potega(p, w - 1);
+}
+
 int main()
 {
-    std::cout << fib(5);
+    std::cout << fib(5)<<std::endl;
+    std::cout << silnia(3)<<std::endl;
+    std::cout << potega(2, 4) << std::endl;
+
+
+
+
 }
